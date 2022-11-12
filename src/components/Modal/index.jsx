7344a70388850190c153e6503226550f73076c85
Leksa <admin@petiteweb.ru>
2022-11-12 20:48:45 +0300

    upd modal

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -5,17 +5,30 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./style.css";
 
 export default ({isActive, changeActive}) => {
+    const [email, setEmail] = useState("");
+    const [pwd, setPwd] = useState("");
+    const handler = e => {
+        e.preventDefault();
+    }
     return <div className={isActive ? "popup-box active" : "popup-box"}>
         <div className="popup">
             <XCircle className="popup-close" onClick={e => {changeActive(false)}}/>
-            <Form>
+            <Form onSubmit={handler}>
                 <Form.Group>
                     <Form.Label>Email</Form.Label>
-                    <Form.Control type="email"/>
+                    <Form.Control 
+                        type="email" 
+                        value={email} 
+                        onChange={e => setEmail(e.target.value)}
+                    />
                 </Form.Group>
                 <Form.Group>
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password"/>
+                    <Form.Control 
+                        type="password" 
+                        value={pwd} 
+                        onChange={e => setPwd(e.target.value)}
+                    />
                 </Form.Group>
                 <Button variant="warning" type="submit">Войти</Button>
             </Form>
